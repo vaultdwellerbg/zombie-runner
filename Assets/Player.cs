0572dd2ab1fa945cbb19d7bcdaccf7b4a582f72c
Vaultdweller <vaultdwellerbg13@gmail.com>
2018-03-21 20:34:40 +0200

    Add player spawn points

---
+++ Assets/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public Transform playerSpawnPoints;
+	public bool respawn = false;
+
+	private Vector3[] spawnPoints;
+
+	void Start () {
+		FindSpawnPoints();
+	}
+
+	private void FindSpawnPoints()
+	{
+		int pointsCount = playerSpawnPoints.childCount;
+		spawnPoints = new Vector3[pointsCount];
+		for (int i = 0; i < pointsCount; i++)
+		{
+			spawnPoints[i] = playerSpawnPoints.GetChild(i).position;
+		}
+	}
+
+	private void Update()
+	{
+		if (respawn)
+		{
+			Respawn();
+			respawn = false;
+		}
+	}
+
+	private void Respawn() {
+		Vector3 randomSpawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+		transform.position = randomSpawnPoint;
+	}
+}
