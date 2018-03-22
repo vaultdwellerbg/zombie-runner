a4f1e70ee209974eac4ab10c4bc2fe2d40fc4d2a
Vaultdweller <vaultdwellerbg13@gmail.com>
2018-03-22 17:11:20 +0200

    Wire-up zoom and call inputs

---
+++ Assets/Scripts/Eyes.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eyes : MonoBehaviour {
+
+	public float zoomRate = 1.5f;
+
+	private Camera eyes;
+	private float defaultFOV;
+
+	void Start () {
+		eyes = GetComponent<Camera>();
+		defaultFOV = eyes.fieldOfView;
+	}
+	
+	void Update () {
+		if (Input.GetButtonDown("Zoom"))
+		{
+			eyes.fieldOfView = defaultFOV / zoomRate;
+		}
+		if (Input.GetButtonUp("Zoom"))
+		{
+			eyes.fieldOfView = defaultFOV;
+		}
+	}
+}
---
+++ Assets/Scripts/Speaker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Speaker : MonoBehaviour {
+
+	private AudioSource audioSource;
+
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	void Update () {
+		if (Input.GetButtonDown("CallHeli"))
+		{
+			audioSource.Play();
+		}
+	}
+}
