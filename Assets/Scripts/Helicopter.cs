919332b40707587876b1aa4e1a8e07c594b8f5fb
Vaultdweller <vaultdwellerbg13@gmail.com>
2018-03-22 17:24:28 +0200

    Add helicopter object and fix line endings

---
+++ Assets/Scripts/Helicopter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helicopter : MonoBehaviour {
+
+	private bool called = false;
+
+	private void Update()
+	{
+		if (Input.GetButtonDown("CallHeli") && !called)
+		{
+			called = true;
+			print("Helicopter called");
+		}
+	}
+}
