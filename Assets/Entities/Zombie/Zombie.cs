cebfb09a866523809c5de6771c9040d114129682
Vaultdweller <vaultdwellerbg13@gmail.com>
2018-03-26 22:18:47 +0300

    Add new NPC type

---
+++ Assets/Entities/Zombie/Zombie.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour {
+
+	private Animator animator;
+
+	private void Start()
+	{
+		animator = GetComponent<Animator>();
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.name == "Player")
+		{
+			animator.SetBool("isAttacking", true);
+		}
+	}
+
+	private void OnCollisionExit(Collision collision)
+	{
+		if (collision.gameObject.name == "Player")
+		{
+			animator.SetBool("isAttacking", false);
+		}
+	}
+}
