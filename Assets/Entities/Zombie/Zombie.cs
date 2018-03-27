78f5b2fe16dc4bc43115c990e669014b8812d648
Vaultdweller <vaultdwellerbg13@gmail.com>
2018-03-27 20:20:06 +0300

    Add NPC voice and refactor script

--- Assets/Entities/Zombie/Zombie.cs
+++ Assets/Entities/Zombie/Zombie.cs
@@ -4,26 +4,61 @@ using UnityEngine;
 
 public class Zombie : MonoBehaviour {
 
+	public AudioClip[] grunts;
+	public AudioClip attackSound;
+
 	private Animator animator;
+	private AudioSource audioSource;
 
 	private void Start()
 	{
 		animator = GetComponent<Animator>();
+		audioSource = GetComponent<AudioSource>();
+		InvokeRepeating("Grunt", 0f, 3f);
+	}
+
+	private void Grunt()
+	{
+		audioSource.Stop();
+		audioSource.clip = grunts[Random.Range(0, grunts.Length)];
+		audioSource.Play();
 	}
 
 	private void OnCollisionEnter(Collision collision)
 	{
 		if (collision.gameObject.name == "Player")
 		{
-			animator.SetBool("isAttacking", true);
+			Attack();
 		}
+	}
+
+	private void Attack()
+	{
+		animator.SetBool("isAttacking", true);
+		CancelInvoke("Grunt");
+		PlayAttackingSound();
+	}
+
+	private void PlayAttackingSound()
+	{
+		audioSource.Stop();
+		audioSource.clip = attackSound;
+		audioSource.loop = true;
+		audioSource.Play();
 	}
 
 	private void OnCollisionExit(Collision collision)
 	{
 		if (collision.gameObject.name == "Player")
 		{
-			animator.SetBool("isAttacking", false);
+			Walk();
 		}
+	}
+
+	private void Walk()
+	{
+		animator.SetBool("isAttacking", false);
+		audioSource.loop = false;
+		InvokeRepeating("Grunt", 0f, 3f);
 	}
 }
