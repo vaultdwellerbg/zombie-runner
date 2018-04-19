ae175107d20116130e746630847e399e967b698c
Vaultdweller <vaultdwellerbg13@gmail.com>
2018-04-19 16:18:10 +0300

    Add day/night cycle

---
+++ Assets/Scripts/DayNightCycle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour {
+
+	public float gameMinutesPerSecond = 1;
+
+	private float anglePerSecond;
+
+	private void Start()
+	{
+		float anglePerGameMinute = 360f / (60f * 24f);
+		anglePerSecond = anglePerGameMinute * gameMinutesPerSecond;
+	}
+
+	void Update () {
+		float angle = anglePerSecond * Time.deltaTime;
+		transform.RotateAround(transform.position, Vector3.forward, angle);
+	}
+}
