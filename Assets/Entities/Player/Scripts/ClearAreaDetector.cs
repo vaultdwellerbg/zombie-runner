afa7006b12f11496d1dd0743443b6a2d3ddb3144
Vaultdweller <vaultdwellerbg13@gmail.com>
2018-04-20 15:30:07 +0300

    Add new prefab and clear area detection

---
+++ Assets/Entities/Player/Scripts/ClearAreaDetector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearAreaDetector : MonoBehaviour {
+
+	private float timeOfLastTrigger;
+	private bool clearZoneFound = false;
+	private AudioSource audioSource;
+
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	void Update () {
+		if (!clearZoneFound && Time.timeSinceLevelLoad - timeOfLastTrigger > 1f)
+		{
+			Debug.Log("Ready");
+			clearZoneFound = true;
+			audioSource.Play();
+		}
+	}
+
+	private void OnTriggerStay(Collider other)
+	{
+		timeOfLastTrigger = Time.timeSinceLevelLoad;
+	}
+}
